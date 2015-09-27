5ec832fa5a4864cbd4b4302224b7a9e1d6931be3
PEM-- <pemarchandet@gmail.com>
2015-09-27 14:52:54 +0200

    0.0.1 - Namespaces

---
+++ packages/namespaces/namespaces.js
@@ -0,0 +1,18 @@
+/** Main namespace */
+SD = {
+  /** Views: React components */
+  Views: {
+    /** React components for the client interface only */
+    Client: {},
+    /** React components for the admin interface only */
+    Admin: {},
+  },
+  /** Collections */
+  Structure: {},
+  /** Menu: Routing, Sitemaps, Human.txt, Robots.txt, ... */
+  Menu: {},
+  /** Permisions: Roles, Accounts, Publication rules, ... */
+  Permission: {},
+  /** Utils: Logger, ES6+ decorators, ... */
+  Utils: {}
+};
